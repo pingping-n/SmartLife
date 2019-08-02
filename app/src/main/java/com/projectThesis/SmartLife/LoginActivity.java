4021e390c71e92037e1ee534d3c8c8027be96541
PANDA_N <Nattapongpanda@gmail.com>
2019-08-02 08:35:56 +0700

    add login with api.

--- app/src/main/java/com/projectThesis/SmartLife/LoginActivity.java
+++ app/src/main/java/com/projectThesis/SmartLife/LoginActivity.java
@@ -3,7 +3,9 @@ package com.projectThesis.SmartLife;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,18 @@ import android.widget.Toast;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,13 +45,8 @@ public class LoginActivity extends AppCompatActivity {
         mPassword = (EditText) findViewById(R.id.input_password);
         mContext = this;
 
-//        MongoClientURI uri = new MongoClientURI(
-//                "mongodb+srv://nattapongping:161135@watcharakornton-n8jsx.gcp.mongodb.net/test?retryWrites=true&w=majority");
-//
-//        MongoClient mongoClient = new MongoClient(uri);
-//        MongoDatabase database = mongoClient.getDatabase("iot-devices");
-//
-//        System.out.println(database);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,8 +61,8 @@ public class LoginActivity extends AppCompatActivity {
         String username = mUsername.getText().toString().trim().toLowerCase();
         String password = mPassword.getText().toString().trim();
 
-//        boolean isSuccess = checkLoginValidate(username, password);
-        boolean isSuccess = true;
+        boolean isSuccess = checkLoginValidate(username, password);
+//        boolean isSuccess = true;
 
         if (isSuccess) {
             Intent intent = new Intent(mContext, MainActivity.class);
@@ -66,8 +75,50 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public boolean checkLoginValidate(String username, String password) {
+        String urlParameters  = "email=" + username + "&password=" + password;
 
+        byte[] postData = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int postDataLength = postData.length;
+        String request = "http://146.148.42.206:4000/api/login";
+        URL url = null;
+        try {
+            url = new URL(request);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            conn.setUseCaches(false);
 
+            try(DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+                wr.write( postData );
+            }
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+            if (response.toString().equals("successful")) {
+                return true;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
